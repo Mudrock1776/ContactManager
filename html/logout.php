7c552c0606096f2a864574abab8c733009610829
JonathanGrovo <129807474+JonathanGrovo@users.noreply.github.com>
2023-09-29 21:51:24 -0400

    Delete html/logout.php

--- html/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start(); //Theses lines are just for destroying the user seesion and sending them back to the login page
-session_unset();
-session_destroy();
-header("Location: index.php");
-exit();
